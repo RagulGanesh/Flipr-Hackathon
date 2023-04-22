e62621ed518da971747a568aa42348a990ce6e85
unknown <ragulpretish@gmail.com>
2023-04-22 12:24:01 +0530

    navbar done

---
+++ index.js
@@ -0,0 +1,17 @@
+const connectToMongo = require("./db");
+const express = require('express')
+var cors = require('cors')
+
+connectToMongo();
+const app = express()
+const port = 5000
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+//Available Routes
+app.use("/api/auth",require('./routes/auth'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
