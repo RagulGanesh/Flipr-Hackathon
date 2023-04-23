cf04e318897f0c738d7c0db950f4c874ab4a187b
Gundaram Raju Rohith <87968757+RohithGundaram@users.noreply.github.com>
2023-04-23 22:06:30 +0530

    done

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors())
 app.use('/public',express.static(path.join(__dirname,'public')))
 
 //Available Routes
+app.use("/api/user", require('./routes/User'))
 app.use("/api/auth",require('./routes/auth'))
 app.use("/api/v1/media",require('./routes/media'))
 
